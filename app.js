473ddf2063fbbf053cd07de954adc936b46777c2
Martin Nordström <martin.nordstrom99@gmail.com>
2016-10-05 17:03:19 +0200

    Made it possible for the user to add their own colors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ $(".controls li").click(function(){
   // Removes the class from it's siblings
   $(this).siblings().removeClass("selected");
   // Adds the class to the color that the user has selected
-  $(this).addClass("selected");
+  $(this).fadeIn(3000).addClass("selected");
   //cache the current color
   color = $(this).css("background-color");
   });
@@ -17,9 +17,20 @@ $(".controls li").click(function(){
 //When click on new color
 $("#revealColorSelect").click(function(){
   // Show color select or hide the color selecte
+  changeColor();
   $("#colorSelect").toggle();
-
 });
+
+// Update color of span
+function changeColor() {
+  var r = $("#red").val();
+  var g = $("#green").val();
+  var b = $("#blue").val();
+  $("#newColor").css("background-color", "rgb(" + r + "," + g +", " + b + ")");
+}
+
+// When color sliders change
+$("input[type=range]").on("input", changeColor);
 
 
 
