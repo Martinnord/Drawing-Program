f4c92ba1b8843ae541fd2a8b7e1c35103b45380a
Martin Nordström <martin.nordstrom99@gmail.com>
2016-10-05 16:02:40 +0200

    Fixed the New Color button so the user can see the options that he/she has when creating a new color.

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ var color = $(".selected").css("background-color");
 
 // Clicking on control list items
 $(".controls li").click(function(){
+  // Removes the class from it's siblings
   $(this).siblings().removeClass("selected");
+  // Adds the class to the color that the user has selected
   $(this).addClass("selected");
-
+  //cache the current color
   color = $(this).css("background-color");
   });
+
+//When click on new color
+$("#revealColorSelect").click(function(){
+  // Show color select or hide the color selecte
+  $("#colorSelect").toggle();
+
+});
 
 
 
