fb01906566240e952e12a81deb7321493a7cecd3
Martin Nordström <martin.nordstrom99@gmail.com>
2016-10-17 03:02:45 +0200

    program is done?

--- app.js
+++ app.js
@@ -3,9 +3,13 @@
 $(document).ready(function(){
 
 var color = $(".selected").css("background-color");
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mousedown = false;
 
 // Clicking on control list items
-$(".controls li").click(function(){
+$(".controls").on("click", "li", function(){
   // Removes the class from it's siblings
   $(this).siblings().removeClass("selected");
   // Adds the class to the color that the user has selected
@@ -30,7 +34,33 @@ function changeColor() {
 }
 
 // When color sliders change
-$("input[type=range]").on("input", changeColor);
+$("input[type=range]").change(changeColor);
+
+// When Add Color is pressed
+$("#addNewColor").click(function(){
+  var $newColor = $("<li></li>");
+  $newColor.css("background-color", $("#newColor").css("background-color"));
+  $(".controls ul").append($newColor);
+  $newColor.click();
+});
+
+$canvas.mousedown(function(e){
+  lastEvent = e;
+  mousedown = true;
+}).mousemove(function(e){
+  if(mousedown) {
+    context.beginPath();
+    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.strokeStyle = color;
+    context.stroke();
+    lastEvent = e;
+  }
+}).mouseup(function(){
+  mousedown = false;
+}).mouseleave(function(){
+  $canvas.mouseup();
+});
 
 
 
